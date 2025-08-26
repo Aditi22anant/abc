eb42a4de2bc960110421f10b8b637f2d2aa09411
Aditi Anant <aditianant22@gmail.com>
2025-08-26 15:42:59 +0530

    First commit

--- index.html/demo.php
+++ index.html/demo.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8" />
+  <title>Login Form</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-    <?php
-    $x=5;
-    $y=10;
-    function myTest(){
-        global $x,$y;
-        $y=$x+$y;
-    }
-    myTest();
-    echo $y;
-   ?>
+  <form method='POST'>
+    <label for="username">Username:</label><br/>
+    <input type="text" id="username" name="username" placeholder="Enter your username" /><br /><br />
+    <input type="submit" value="Submit" />
+  </form>
+  <?php
+  if ($_SERVER["REQUEST METHOD"] == "POST" && isset($_POST['username'])) {
+    $username = $_POST['username'];
+    echo "<h3> Hello $username </h3>";
+}
+?>
 </body>
 </html>
---
+++ index.html/table.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Table</title>
+</head>
+<body>
+    <?php
+    echo "<table border ='1'>
+    <tr>
+    <td> salary of mr. A is </td>
+    <td> 1000$ </td>
+    </tr>
+    
+    <tr>
+    <td> salary of mr. B is </td>
+    <td> 2000$ <td>
+    </tr>
+
+    <tr>
+    <td> salary of mr. C is </td>
+    <td> 2300$ </td>
+    </tr>
+
+    </table>";
+    ?>
+    </body>
+    </html>
